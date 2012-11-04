a8cf622a8166d10968bb62a6f4b518f8e1d087ce
Lais Andrade <lsa@cin.ufpe.br>
2012-11-04 16:39:18 -0300

    Fixing css and javascript problems for Chrome

--- webapp/js/suggest.js
+++ webapp/js/suggest.js
@@ -1,6 +1,6 @@
 
 function add_author(){
-	var author = $('.author-input').clone();
+	var author = $('.author-input:first').clone();
 	author.css('display', 'block');
 	$('.authors').prepend(author);
 	
