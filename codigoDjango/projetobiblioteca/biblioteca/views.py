5d6476c2ef787c1093bce3a9c367eb1c5babbfab
rvbc <ruanvbc@gmail.com>
2012-11-24 19:02:34 -0300

    Formulario add_sugestao
    Foi adicionada mais uma variável em settings.py: "TEMPLATE_DIRS".
    Cada um tem que colocar o caminho do diretório do seu pc.
    
    Para acessar a view basta acessar a url:
    "http://localhost:8000/biblioteca/suggestion"
    É preciso adicionar as funcionalidades dos botoes e do BD na página (a
    página tá "morta").

--- codigoDjango/projetobiblioteca/biblioteca/views.py
+++ codigoDjango/projetobiblioteca/biblioteca/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from biblioteca.models import Book, User, Writer, Suggestion
+from django.http import HttpResponse
+import django.template.loader
+import django.template  
+
+def add_suggestion(request):
+    t = django.template.loader.get_template("add_sugestao.html")
+    c = django.template.Context()
+    return HttpResponse(t.render(c))
--- codigoDjango/projetobiblioteca/projetobiblioteca/urls.py
+++ codigoDjango/projetobiblioteca/projetobiblioteca/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'projetobiblioteca.views.home', name='home'),
     # url(r'^projetobiblioteca/', include('projetobiblioteca.foo.urls')),
-
+    url(r'^biblioteca/suggestion', 'biblioteca.views.add_suggestion'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
