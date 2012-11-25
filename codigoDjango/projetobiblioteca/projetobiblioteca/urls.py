0cfce779e0889ea358aee38c64e20fd0c0c61c57
rvbc <ruanvbc@gmail.com>
2012-11-25 18:58:54 -0300

    Ataque csfr
    Comentei a classe "django.middleware.csrf.CsrfViewMiddleware" no
    settings.py para poder usar formulários post.

--- codigoDjango/projetobiblioteca/projetobiblioteca/urls.py
+++ codigoDjango/projetobiblioteca/projetobiblioteca/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from biblioteca import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,9 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('biblioteca.views',
     # Examples:
     url(r'^biblioteca/$', 'home', name='home'),
-    url(r'^biblioteca/suggestion', 'suggestion'),
+    url(r'^biblioteca/suggestion/$', 'suggestion'),
     url(r'^biblioteca/books', 'books'),
-
+    url(r'^biblioteca/suggestion/add', views.add_suggestion),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
