362ab27e2c7fc560e7ece68483d2fd712388e5d8
Lais Andrade <lsa@cin.ufpe.br>
2012-12-01 15:42:20 -0300

    Javascript changed for multiple authors

--- codigoDjango/projetobiblioteca/biblioteca/static/js/suggest.js
+++ codigoDjango/projetobiblioteca/biblioteca/static/js/suggest.js
@@ -1,9 +1,11 @@
 
 function add_author(){
-	var author = $('.author-input:first').clone();
+	var author = $('.author-input:last').clone();
+	var ind = parseInt(author.attr('name').substring('escritor_'.length)) + 1;
 	author.css('display', 'inline-block');
 	author.val('');
-
+	author.attr('name', 'escritor_'+ind);
+	
 	$('.author-input').css('display', 'block');
 	$('.authors .add-button').before(author);
 	
