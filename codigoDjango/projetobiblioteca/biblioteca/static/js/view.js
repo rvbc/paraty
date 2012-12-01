e2a6957dea9f4d532260e2794b3db73cd14d9da3
Lais Andrade <lsa@cin.ufpe.br>
2012-12-01 12:58:04 -0300

    Some other changes

--- codigoDjango/projetobiblioteca/biblioteca/static/js/view.js
+++ codigoDjango/projetobiblioteca/biblioteca/static/js/view.js
@@ -1,6 +1,14 @@
 
 function accordion_change(pos){
-	var margin = pos * 40;
+	var h = parseInt($('.main_content .accordion h3').height());
+	h = h + parseInt($('.main_content .accordion h3').css('margin-top').replace('px', ''));
+	h = h + parseInt($('.main_content .accordion h3').css('margin-bottom').replace('px', ''));
+	h = h + parseInt($('.main_content .accordion h3').css('padding-bottom').replace('px', ''));
+	h = h + parseInt($('.main_content .accordion h3').css('padding-top').replace('px', ''));
+	h = h + parseInt($('.main_content .accordion h3').css('border-top-width').replace('px', ''));
+	h = h + parseInt($('.main_content .accordion h3').css('border-bottom-width').replace('px', ''));
+	
+	var margin = pos * h;
 	$('.side_menu').css('margin-top', margin+'px');
 }
 
--- codigoDjango/projetobiblioteca/biblioteca/views.py
+++ codigoDjango/projetobiblioteca/biblioteca/views.py
@@ -69,12 +69,11 @@ def add_suggestion(request):
         return TemplateResponse(request, 'suggestion.html', {'error': errorMessage, 'form':form})
 
 def search(request):
-    books, suggestions = models.searchSuggestion(str(request.GET['field']))
+    books, suggestions = models.searchSuggestion(str(request.GET['q']))
 
     if len(books) == 0:#Nothing found!
         #colocar uma mensagem de erro. Mas eh melhor arrumar antes as mensagens de base.html
-        return HttpResponse('Nada foi encontrado!')
+        return TemplateResponse(request, 'books.html', {'group_book_list': books, 'group_suggestion_list':suggestions, 'msg': {'title' : 'Hey', 'content' : 'Nenhum livro foi encontrado nesta pesquisa. Tente com outros termos.'}})
     else:#Display results at books.html
         return TemplateResponse(request, 'books.html', {'group_book_list': books, 'group_suggestion_list':suggestions})
 
-    
