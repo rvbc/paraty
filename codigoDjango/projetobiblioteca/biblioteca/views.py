8ef46cf7be6dd4be978eb52f86fdb899845f02eb
rvbc <ruanvbc@gmail.com>
2012-12-02 09:25:13 -0300

    Forms na mesma url
    Quando submetemos um form, ele mantém a url de origem.

--- codigoDjango/projetobiblioteca/biblioteca/views.py
+++ codigoDjango/projetobiblioteca/biblioteca/views.py
@@ -18,14 +18,20 @@ def home(request):
     return HttpResponse(t.render(c))
 
 def suggestion(request):
-    t = django.template.loader.get_template("suggestion.html")
-    c = django.template.Context()
-    return HttpResponse(t.render(c))
+    if request.method == 'POST':#form submitted
+        return add_suggestion(request)
+    else:#suggestion's home
+        t = django.template.loader.get_template("suggestion.html")
+        c = django.template.Context()
+        return HttpResponse(t.render(c))
     
 def books(request):
-    t = django.template.loader.get_template("books.html")
-    c = django.template.Context()
-    return HttpResponse(t.render(c))
+    if request.method == 'POST':#export excel!
+        return export(request)
+    else:#books' home
+        t = django.template.loader.get_template("books.html")
+        c = django.template.Context()
+        return HttpResponse(t.render(c))
 
     #if request.method == 'POST'
 
