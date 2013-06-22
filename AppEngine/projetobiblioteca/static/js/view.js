892d02944cdb41930a69c99913306ea62070294a
laissandrade <lsa@cin.ufpe.br>
2013-06-22 13:45:01 -0300

    Changes done in Suggestions component in books.html

--- AppEngine/projetobiblioteca/static/js/view.js
+++ AppEngine/projetobiblioteca/static/js/view.js
@@ -6,22 +6,23 @@ function exportButton(){
 
 function toggleSuggestions(){
 	var id = $(this).attr('alt');
-	var s = $('.ui-icon .'+id);
-	alert(id);
+	var s = $('.ui-icon.'+id);
+	
 	if(s.hasClass('ui-icon-triangle-1-e')){
 		s.removeClass('ui-icon-triangle-1-e');
 		s.addClass('ui-icon-triangle-1-s');
+		$('#'+id).slideDown();
 	} else {
 		s.removeClass('ui-icon-triangle-1-s');
 		s.addClass('ui-icon-triangle-1-e');
+		$('#'+id).slideUp();
 	}
-	
-	$('#'+id).slideToggle();
 }
 
 $(
 	function () {
 		$('.header .radioset #books').click();
 		$('.suggestions .head').click(toggleSuggestions);
+		$('.ui-accordion-content').css('height', 'auto');
 	}
 );
