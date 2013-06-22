f1cfc6e8d50dee41ae3e024853225efc95764a3b
laissandrade <lsa@cin.ufpe.br>
2013-06-22 17:38:43 -0300

    nonrel search done.

--- AppEngine/projetobiblioteca/biblioteca/search_indexes.py
+++ AppEngine/projetobiblioteca/biblioteca/search_indexes.py
@@ -2,6 +2,5 @@ import search
 from search.core import porter_stemmer_non_stop
 from biblioteca.models import Book, Writer
 
-search.register(Book, ('title', 'publisher','isbn', 'year', ), indexer=porter_stemmer_non_stop)
-search.register(Writer, ('name', ), indexer=porter_stemmer_non_stop)
+search.register(Book, ('title', 'publisher','isbn', 'year', 'authors', ), indexer=porter_stemmer_non_stop)
 
